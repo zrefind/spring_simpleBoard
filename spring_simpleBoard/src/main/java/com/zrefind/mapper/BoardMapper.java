b8edb301367a3160353679931076745b2df6edcb
zrefind <zrefind@gmail.com>
2019-04-30 23:44:03 +0900

    [CRUD] getList()

---
+++ spring_simpleBoard/src/main/java/com/zrefind/mapper/BoardMapper.java
@@ -0,0 +1,11 @@
+package com.zrefind.mapper;
+
+import java.util.List;
+
+import com.zrefind.domain.ArticleVO;
+
+public interface BoardMapper {
+	
+	public List<ArticleVO> getList();
+
+}
---
+++ spring_simpleBoard/src/test/java/com/zrefind/mapper/BoardMapperTest.java
@@ -0,0 +1,25 @@
+package com.zrefind.mapper;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import lombok.Setter;
+import lombok.extern.log4j.Log4j;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
+@Log4j
+public class BoardMapperTest {
+	
+	@Setter(onMethod_ = @Autowired)
+	private BoardMapper mapper;
+	
+	@Test
+	public void test_getList() {
+		mapper.getList().forEach(article -> log.info(article));
+	}
+
+}
